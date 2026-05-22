4877617b251bbb65eef30c6a3d8c61a3dd552be6
SirW1zz <justinjovenm@gmail.com>
2026-05-22 18:31:32 +0530

    restructing the directory

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require("express");
+import express from "express"
+import newroutes from "./routes/newroutes.js";
 
 const app = express();
+
+app.use("/home",newroutes);
 
 app.listen(5001, () =>{
 console.log("Server started on port 5001");
